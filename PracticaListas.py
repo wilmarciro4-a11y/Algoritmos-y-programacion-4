b15b7045b55ce3143932b4ff12789052da39fdd4
wilmar ciro <wilmarciro4@gmail.com>
2026-02-05 20:27:51 -0500

    Practicas de listas

---
+++ PracticaListas.py
@@ -0,0 +1,46 @@
+class Nodo:
+    def __init__(self, documento, nombre):
+        self.documento = documento
+        self.nombre = nombre
+        self.siguiente = None
+
+class Lista:
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+    
+    def agregar_al_final(self, documento, nombre):
+        nodo = Nodo(documento, nombre)
+        
+        if self.cabeza == None:
+            self.cabeza = nodo
+            self.cola = nodo
+        else:
+            self.cola.siguiente = nodo
+            self.cola = nodo    
+
+
+
+
+class Nodo:
+    def __init__(self, documento, nombre):
+        self.documento = documento
+        self.nombre = nombre
+        self.siguiente = None
+
+class Lista:
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+    
+    def agregar_al_final(self, documento, nombre):
+        nodo = Nodo(documento, nombre)
+        
+        if self.cabeza == None:
+            self.cabeza = nodo
+            
+        else:
+            actual = self.cabeza
+            while actual.siguiente != None:
+                actual = actual.siguiente
+            actual.siguiente = nodo                    
